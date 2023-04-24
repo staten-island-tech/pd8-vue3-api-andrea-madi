ac832b610d295dd871747f4f081df9979ccd1df1
andreaguo <andrea.guo2007@gmail.com>
2023-04-24 13:51:03 -0400

    sdfgs

---
+++ sat-project/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import {ref, onMounted} from 'vue'
+const api = ref('') 
+async function getAPI(){
+  let res = await fetch ('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  let data = await res.json()
+  api.value = data.results
+  console.log(data)
+}
+onMounted(()=> {
+getAPI()
+})
+
+</script>
+
+<template>
+  <main>
+
+  </main>
+</template>
