f1d127ba8a53ee46431f18322202bae3dcdb3bfc
madisonmeccia <madisonnicole317@gmail.com>
2023-04-26 14:01:34 -0400

    gg

--- sat-project/src/App.vue
+++ sat-project/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Chartjs from './views/Chartjs.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -16,8 +15,8 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
   <RouterView />
+  <Chartjs />
 </template>
 
 <style scoped>
