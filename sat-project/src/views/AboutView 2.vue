9b6d8e7c61f5831fed2315e6f3e939702ccb7fe5
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-05-01 18:07:16 -0400

    d

---
+++ sat-project/src/views/AboutView 2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <DoughnutChart class="chart"/>
+
+  </div>
+</template>
+<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
+
+<script>
+  import DoughnutChart from '../components/DoughnutChart.vue'
+  export default {
+  name: 'About',
+  components: { DoughnutChart }
+}
+
+</script>
+
+<style>
+
+*, body{
+  font: black;
+}
+
+.chart{
+  display: flex;
+  justify-content: center;
+  height: 90vh;
+}
+</style>
