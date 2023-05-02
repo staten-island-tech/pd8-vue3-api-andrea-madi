ff36a8434d0b455aa5b05d70ec023737a395a36c
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-05-01 23:40:18 -0400

    b

---
+++ sat-project/src/views/Doughnut.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>This will be andrea's doughnut</header>
+</template>
+
+<style>
+*,
+body,
+html {
+  text-align: center;
+  font: 2rem;
+  margin: 0;
+  padding: 0;
+}
+</style>
